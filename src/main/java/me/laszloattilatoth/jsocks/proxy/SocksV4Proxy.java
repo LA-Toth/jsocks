1c8d0deefa60c35547b1094889a2d2f5818e7b2a
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2021-03-12 15:05:21 +0100

    SOCKSv4: add support of SOCKS4a

--- src/main/java/me/laszloattilatoth/jsocks/proxy/SocksV4Proxy.java
+++ src/main/java/me/laszloattilatoth/jsocks/proxy/SocksV4Proxy.java
@@ -10,6 +10,7 @@ import java.util.logging.Logger;
 public class SocksV4Proxy extends SocksProxy {
     private final byte[] portBytes = new byte[2];
     private final byte[] addrBytes = new byte[4];
+    private boolean socksV4A = false;
 
     SocksV4Proxy(SocketChannel s, Logger logger, String name, int threadId) {
         super(s, logger, name, threadId);
@@ -25,8 +26,26 @@ public class SocksV4Proxy extends SocksProxy {
         if (inputStream.read(portBytes) <= 0 || inputStream.read(addrBytes) <= 0 || inputStream.read() < 0)
             return;
 
+        socksV4A = addrBytes[0] == 0 && addrBytes[1] == 0 && addrBytes[2] == 0 && addrBytes[3] != 0;
+
         short port = ByteBuffer.wrap(portBytes).getShort();
-        InetAddress address = InetAddress.getByAddress(addrBytes);
+        InetAddress address = null;
+        if (socksV4A) {
+            int b;
+            StringBuilder buffer = new StringBuilder();
+            while ((b = inputStream.read()) > 0) {
+                buffer.append((char) b);
+            }
+            if (b == -1) {
+                // SOCKS4a client issue, pointless to send anything
+                return;
+            }
+
+            address = InetAddress.getByName(buffer.toString());
+            logger.info(() -> String.format("Receiving SOCKS4a request; address='%s'", buffer.toString()));
+        } else {
+            address = InetAddress.getByAddress(addrBytes);
+        }
 
         connectAndTransfer(address, port);
     }
